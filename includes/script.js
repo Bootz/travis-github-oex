ebda0378c215a4fad31000860f1fc061f7f5f332
Smasty <martin@smasty.net>
2012-06-24 19:31:59 +0200

    Version 2.0: Support user pages + better design

--- includes/script.js
+++ includes/script.js
@@ -4,29 +4,37 @@
 
 window.addEventListener('DOMContentLoaded', function(){
 
+	// Project page
 	var el = window.document.querySelector('.title-actions-bar h1 strong');
 	if(el){
-		insertBuildStatus(el);
+		insertBuildStatus(el, window.location.pathname.split('/').splice(0,3).join('/'), '0 0 0 8px');
 	}
 
-	function insertBuildStatus(el){
-	    var project = window.location.pathname.split('/').splice(0,3).join('/');
+	// User page
+	var links = document.querySelectorAll('.repo_list li h3');
+	for(var i = 0, len = links.length; i < len; i++){
+		var project = '/' + links[i].querySelector('a').href.split('/').splice(3,3).join('/');
+		console.log(project);
+		insertBuildStatus(links[i], project, '2px 0 0');
+	}
 
-	    var img = window.document.createElement('img');
-	    img.src = 'https://secure.travis-ci.org' + project + '.png';
-	    img.alt = 'build status';
-	    img.onload = function(){
-	        document.styleSheets[0].insertRule('#travis-ci{border-radius:1px;display:inline-block;margin:0 -1px 0 8px;opacity:.9}', 1);
-	        document.styleSheets[0].insertRule('#travis-ci:hover{background:rgba(0,0,0,.5);box-shadow: 0 0 3px rgba(0,0,0,.5);opacity:1;cursor:pointer}', 1);
-	        document.styleSheets[0].insertRule('#travis-ci img{display:block}', 1);
+	function insertBuildStatus(el, project, margin){
+		var img = window.document.createElement('img');
+		img.src = 'https://secure.travis-ci.org' + project + '.png';
+		img.alt = 'build status';
+		img.onload = function(){
+			document.styleSheets[0].insertRule('.travis-ci{z-index:50;position:absolute;margin:' + margin + ';opacity:.5;overflow:hidden}', 1);
+			document.styleSheets[0].insertRule('.travis-ci:hover{opacity:1;cursor:pointer}', 1);
+			document.styleSheets[0].insertRule('.travis-ci img{position:relative;left:-53px;-o-transition:all .3s}', 1);
+			document.styleSheets[0].insertRule('.travis-ci:hover img{left:0}', 1);
 
-	        var link = window.document.createElement('a');
-	        link.href = 'http://travis-ci.org' + project;
-	        link.id = 'travis-ci';
+			var link = window.document.createElement('a');
+			link.href = 'http://travis-ci.org' + project;
+			link.className = 'travis-ci';
 
-	        link.appendChild(img);
-        	el.appendChild(link);
-    	}
+			link.appendChild(img);
+			el.appendChild(link);
+		}
 	}
 
 }, false);
